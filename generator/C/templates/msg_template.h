15ca1d9656b94e02a98dcde70569d3fe0da2d304
olliw42 <waldmanns@gmx.de>
2022-12-03 09:09:57 +0100

    bug: msg_xxx_decode(), if message has unknown extensions msg->len can be larger PAYLOAD_LEN_MAX, and copying with msg->len would write beyond allocated memory

--- generator/C/templates/msg_template.h
+++ generator/C/templates/msg_template.h
@@ -181,10 +181,13 @@ FASTMAVLINK_FUNCTION_DECORATOR uint16_t fmav_msg_${name_lower}_encode_to_serial(
 FASTMAVLINK_FUNCTION_DECORATOR void fmav_msg_${name_lower}_decode(fmav_${name_lower}_t* payload, const fmav_message_t* msg)
 {
 #if FASTMAVLINK_ALWAYS_ZEROFILL
-    memcpy(payload, msg->payload, msg->len);
-    // ensure that returned payload is zero filled
     if (msg->len < FASTMAVLINK_MSG_${name}_PAYLOAD_LEN_MAX) {
+        memcpy(payload, msg->payload, msg->len);
+        // ensure that returned payload is zero filled
         memset(&(((uint8_t*)payload)[msg->len]), 0, FASTMAVLINK_MSG_${name}_PAYLOAD_LEN_MAX - msg->len);
+    } else {
+		// note: msg->len can be larger than PAYLOAD_LEN_MAX if the message has unknown extensions
+        memcpy(payload, msg->payload, FASTMAVLINK_MSG_${name}_PAYLOAD_LEN_MAX);
     }
 #else
     // this requires that msg payload had been zero filled before
